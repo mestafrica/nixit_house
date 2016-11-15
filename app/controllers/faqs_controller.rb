d9e6fc32ca356a3183947d3d8f6e6477818a3cec
josoftme <joshuaiheji@gmail.com>
2016-11-15 16:04:53 +0100

    Create the FAQ Entity
    This creates an Faq model, Faqs controller and the CRUD views for an FAQ
    (Frequently Asked Question). A user can add an FAQ, edit it and delete it.
    All the created FAQs can be seen on .../faqs.

---
+++ app/controllers/faqs_controller.rb
@@ -0,0 +1,51 @@
+class FaqsController < ApplicationController
+
+	def index
+		@faqs = Faq.all
+	end
+
+	def show
+		@faq = Faq.find(params[:id])
+	end
+
+	def new
+		@faq = Faq.new
+	end
+
+	def edit
+	  	@faq = Faq.find(params[:id])
+	end
+
+	def create
+		@faq = Faq.new(params.require(:faq).permit(:question, :answer))
+		
+		if @faq.save
+			redirect_to @faq
+		else
+			render 'new'
+		end
+	end
+
+	def update
+	  @faq = Faq.find(params[:id])
+	 
+	  if @faq.update(faq_params)
+	    redirect_to @faq
+	  else
+	    render 'edit'
+	  end
+	end
+
+	def destroy
+	  @faq = Faq.find(params[:id])
+	  @faq.destroy
+	 
+	  redirect_to faq_path
+	end
+
+	private
+		def faq_params
+			params.require(:faq).permit(:question, :answer)
+		end
+
+end
